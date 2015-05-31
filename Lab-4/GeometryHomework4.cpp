d240f3a83d78ba462f732791b7158f1f1420264d
Haris Hanif <hhanif@users.noreply.github.com>
2015-05-31 11:15:09 -0700

    GeometryHomework4.cpp

---
+++ Lab-4/GeometryHomework4.cpp
@@ -0,0 +1,270 @@
+// Lab 4B, Geometrey Homework Program
+// Programmer: Haris Hanif
+// Editor(s) used: Xcode
+// Compiler(s) used: G++
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::ios;
+#include <iomanip>
+using std::setprecision;
+#include <cstdlib>
+
+#include <fstream>
+using std::ifstream;
+
+#include <cstring>
+
+//Classes
+class Square {
+private:
+    const double square_side;
+public:
+    Square(const char* const token[4]):square_side(token[1]==0?0:atof(token[1]))
+
+    {
+
+    }
+    void output() const;
+};
+
+class Rectangle {
+private:
+    const double rectangle_length;
+    const double rectangle_width;
+public:
+    Rectangle(const char* const token[4]):rectangle_length(token[1] == 0?0:atof(token[1])), rectangle_width(token[2] == 0?0:atof(token[2]))
+    {
+
+    }
+    void output() const;
+};
+class Circle {
+private:
+    const double circle_radius;
+public:
+    Circle(const char* const token[4]):circle_radius(token[1] == 0?0:atof(token[1]))
+    {
+
+    }
+    void output() const;
+};
+class Cube {
+private:
+    const double cube_side;
+public:
+    Cube(const char* const token[4]):cube_side(token[1] == 0?0:atof(token[1]))
+    {
+        
+    }
+    void output() const;
+};
+class Prism {
+private:
+    const double prism_length;
+    const double prism_width;
+    const double prism_height;
+public:
+    Prism(const char* const token[4]):prism_length(token[1] == 0?0:atof(token[1])), prism_width(token[2] == 0?0:atof(token[2])), prism_height(token[3] == 0?0:atof(token[3]))
+    {
+        
+    }
+    void output() const;
+    
+};
+class Cylinder {
+private:
+    const double cylinder_radius;
+    const double cylinder_height;
+public:
+    Cylinder(const char* const token[4]):cylinder_radius(token[1] == 0?0:atof(token[1])), cylinder_height(token[2] == 0?0:atof(token[2]))
+    {
+
+    }
+    void output() const;
+    
+};
+
+
+
+const int MAX_CHARS_PER_LINE = 512;
+const int MAX_TOKENS_PER_LINE = 20;
+const char* const DELIMITER = " ";
+const double PI = 3.14159265;
+
+int main()
+{
+    // print my name and this assignment's title
+    cout << "Lab 4B, Geometrey Homework Program \n";
+    cout << "Programmer: Haris Hanif\n";
+    cout << "Editor(s) used: Xcode\n";
+    cout << "Compiler(s) used: G++\n";
+    cout << "File: " << __FILE__ << endl;
+    cout << "Complied: " << __DATE__ << " at " << __TIME__ << endl << endl;
+    
+    // create a file-reading object
+    ifstream fin;
+    const void *shape[100];
+    int ShapeId[100];
+    int ShapeIndex = 0;
+    
+    fin.open("geo.txt"); // open a file
+    if (!fin.good())
+    {
+        cout << "geo.txt did not open\n";
+        return 1; // exit if file not found
+    }
+    else
+    {
+        cout << "Opened file successfully\n";
+    }
+    
+    // read each line of the file
+    while (!fin.eof())
+    {
+        // read an entire line into memory
+        char buf[MAX_CHARS_PER_LINE];
+        fin.getline(buf, MAX_CHARS_PER_LINE);
+        
+        // parse the line into blank-delimited tokens
+        int n = 0; // a for-loop index
+        
+        // array to store memory addresses of the tokens in buf
+        const char* token[MAX_TOKENS_PER_LINE] = {}; // initialize to 0
+        
+        // parse the line
+        token[0] = strtok(buf, DELIMITER); // first token
+        if (token[n]) // zero if line is blank
+        {
+            for (n = 1; n < MAX_TOKENS_PER_LINE; n++)
+            {
+                token[n] = strtok(0, DELIMITER); // subsequent tokens
+                if (!token[n]) break; // no more tokens
+            }
+
+            //Print and Calculate Square Values
+            if(strcmp(token[0],"SQUARE")==0){
+                const Square* const s = new Square(token);
+                shape[ShapeIndex] =s;
+                ShapeId[ShapeIndex] = 1;
+                ShapeIndex++;
+            }
+            //Print and Calculate Rectangle Values
+            else if(strcmp(token[0],"RECTANGLE") == 0){
+                const Rectangle* const r = new Rectangle(token);
+                shape[ShapeIndex] = r;
+                ShapeId[ShapeIndex] = 2;
+                ShapeIndex++;
+            }
+            //Print and Calculate Circle Values
+            else if(strcmp(token[0],"CIRCLE") == 0){
+                const Circle* const c = new Circle(token);
+                shape[ShapeIndex] = c;
+                ShapeId[ShapeIndex] = 3;
+                ShapeIndex++;
+            }
+            //Print and Calculate Cube Values
+            else if(strcmp(token[0],"CUBE") == 0){
+                const Cube* const cu = new Cube(token);
+                shape[ShapeIndex] = cu;
+                ShapeId[ShapeIndex] = 4;
+                ShapeIndex++;
+            }
+            //Print and Calculate Rectangular Prism Values
+            else if(strcmp(token[0],"PRISM") == 0){
+                const Prism* const p = new Prism(token);
+                shape[ShapeIndex] = p;
+                ShapeId[ShapeIndex] = 5;
+                ShapeIndex++;
+            }
+            //Print and Calculate Cylinder Values
+            else if(strcmp(token[0],"CYLINDER") == 0){
+                const Cylinder* const cyl = new Cylinder(token);
+                shape[ShapeIndex] = cyl;
+                ShapeId[ShapeIndex] = 6;
+                ShapeIndex++;
+            }
+            else {
+                cout<< token[0] << " Invalid Object Input" << endl;
+            }
+        }
+    }
+    
+    fin.close();
+    
+    for(int a=0; a<ShapeIndex; a++){
+        if(ShapeId[a] == 1) ((Square*)shape[a])->output();
+        else if(ShapeId[a] == 2) ((Rectangle*)shape[a])->output();
+        else if(ShapeId[a] == 3) ((Circle*)shape[a])->output();
+        else if(ShapeId[a] == 4) ((Cube*)shape[a])->output();
+        else if(ShapeId[a] == 5) ((Prism*)shape[a])->output();
+        else if(ShapeId[a] == 6) ((Cylinder*)shape[a])->output();
+    }
+    for(int b=0; b<ShapeIndex; b++){
+        if(ShapeId[b] == 1) delete((Square*)shape[b]);
+        else if(ShapeId[b] == 2) delete((Rectangle*)shape[b]);
+        else if(ShapeId[b] == 3) delete((Circle*)shape[b]);
+        else if(ShapeId[b] == 4) delete((Cube*)shape[b]);
+        else if(ShapeId[b] == 5) delete((Prism*)shape[b]);
+        else if(ShapeId[b] == 6) delete((Cylinder*)shape[b]);
+    }
+    
+    cout<<"All objects have been erased"<<endl;
+}
+
+
+//Function Definitions
+void Square::output() const{
+    cout.unsetf(ios::fixed|ios::showpoint);
+    cout << setprecision(6);
+    cout <<"SQUARE side="<< square_side;
+    cout.setf(ios::fixed|ios::showpoint);
+    cout << setprecision(2);
+    cout <<" area = " << square_side * square_side << " perimeter = " << square_side * 4 <<endl;
+    
+}
+void Rectangle::output() const{
+    cout.unsetf(ios::fixed|ios::showpoint);
+    cout << setprecision(6);
+    cout << "RECTANGLE length =" <<rectangle_length<<" width = " << rectangle_width;
+    cout.setf(ios::fixed|ios::showpoint);
+    cout << setprecision(2);
+    cout <<" area = " << rectangle_length * rectangle_width << " perimeter = " << (2 * rectangle_length) + (2 * rectangle_width) << endl;
+}
+void Circle::output() const{
+    cout.unsetf(ios::fixed|ios::showpoint);
+    cout << setprecision(6);
+    cout <<" CIRCLE radius = " << circle_radius;
+    cout.setf(ios::fixed|ios::showpoint);
+    cout << setprecision(2);
+    cout <<"area = " << (PI) * ((circle_radius) * (circle_radius)) << " circumference = " << 2 * PI * circle_radius <<endl;
+    
+}
+void Cube::output() const{
+    cout.unsetf(ios::fixed|ios::showpoint);
+    cout << setprecision(6);
+    cout <<"CUBE side = " << cube_side;
+    cout.setf(ios::fixed|ios::showpoint);
+    cout << setprecision(2);
+    cout<<" surface area = " << 6 * (cube_side * cube_side) << " volume = " << cube_side * cube_side * cube_side <<endl;
+    
+}
+void Prism::output() const{
+    cout.unsetf(ios::fixed|ios::showpoint);
+    cout << setprecision(6);
+    cout <<"PRISM length = "<<prism_length << " width = " << prism_width << " height = " << prism_height;
+    cout.setf(ios::fixed|ios::showpoint);
+    cout << setprecision(2);
+    cout <<" surface Area = " << (2*prism_length*prism_width) + (2*prism_width*prism_height) + (2*prism_length*prism_height) << " volume = " << prism_length*prism_width*prism_height <<endl;
+    
+}
+void Cylinder::output() const{
+    cout.unsetf(ios::fixed|ios::showpoint);
+    cout << setprecision(6);
+    cout <<"CYLINDER radius = " << cylinder_radius << " height = " << cylinder_height;
+    cout.setf(ios::fixed|ios::showpoint);
+    cout << setprecision(2);
+    cout <<" surface area = " << 2*(PI)*(cylinder_radius*cylinder_radius) + 2*(PI)*(cylinder_radius*cylinder_height) << " volume = " << (PI) * (cylinder_radius*cylinder_radius) * cylinder_height << endl;
+    
+}
